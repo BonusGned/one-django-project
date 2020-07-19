a74fd15a1a6af37e85ee0065007ff72a221e7407
BonusGned <jesus.moj@mail.ru>
2020-07-19 14:22:30 +0300

    sql, upd model

--- news/views.py
+++ news/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import News
+
 def index(request):
-    print(request)
-    return HttpResponse('Hello World')
+    news = News.objects.all()
+    context = {
+        'news': news,
+        'title': 'Список новостей',
+    }
+    return render(request, template_name='news/index.html', context=context)
 
 def test(request):
     return HttpResponse('<h1>Тест страница</h1>')
