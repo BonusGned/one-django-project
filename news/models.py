a6600456caba5a3dca9c652ebb7c1966987d6534
BonusGned <jesus.moj@mail.ru>
2020-08-04 17:14:44 +0300

    orm

--- news/models.py
+++ news/models.py
@@ -8,7 +8,8 @@ class News(models.Model):
     update_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Изображение', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Состояние публикации')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория', related_name='get_news')
+    views = models.IntegerField(default=0)
 
     def get_absolute_url(self):
         return reverse_lazy('view_news', kwargs={"pk": self.pk})
--- news/views.py
+++ news/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+
 
 from .models import News, Category
 from .forms import NewsForm
@@ -38,6 +40,11 @@ class ViewNews(DetailView):
     #template_name = 'news/news_detail.html'
     context_object_name = 'news_item'
 
+class CreateNews(CreateView):
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
+    success_url = reverse_lazy('home')
+
 
 '''def index(request):
     news = News.objects.all()
@@ -59,7 +66,7 @@ def view_news(request, news_id):
     news_item = get_object_or_404(News, pk=news_id)
     return render(request, 'news/view_news.html', {"news_item": news_item})
 
-def add_news(request):
+'''def add_news(request):
     if request.method == 'POST':
         form = NewsForm(request.POST)
         if form.is_valid():
@@ -68,4 +75,4 @@ def add_news(request):
             return redirect(news)
     else:
         form = NewsForm()
-    return render(request, 'news/add_news.html', {'form': form})
+    return render(request, 'news/add_news.html', {'form': form})'''
