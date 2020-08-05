c93bd4599a96dbe3d0040a53611776ebe7b3b935
BonusGned <jesus.moj@mail.ru>
2020-08-05 17:55:51 +0300

    Mixin, custadmin

--- testsite/urls.py
+++ testsite/urls.py
@@ -18,8 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from news .views import *
-
+from news.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +26,9 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+                      path('__debug__/', include(debug_toolbar.urls)),
+                  ] + urlpatterns
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
